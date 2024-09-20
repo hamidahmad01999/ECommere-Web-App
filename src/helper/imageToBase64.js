d6d35885fa3b20feaeae18c8ee2d353a7731ae50
unknown <hamidahmad01999@gmail.com>
2024-09-20 15:13:07 +0500

    Helper function to convert image to base64 which is used to convert image into string

---
+++ src/helper/imageToBase64.js
@@ -0,0 +1,14 @@
+const imageToBase64=(image)=>{
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+
+    const data=new Promise((resolve, reject)=>{
+        reader.onload=()=>resolve(reader.result)
+
+        reader.onerror=error=>reject(error);
+    })
+
+    return data
+}
+
+export default imageToBase64;
